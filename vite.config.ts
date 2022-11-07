4c7b08e31e0368cdfd210f1cd6d3436c120c41c5
pengrenchang <pengrch@activeio.com.cn>
2022-11-07 11:42:08 +0800

    fix: 初始化项目

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+/**
+ * @description:
+ * @author: pengrenchang
+ * @Date: 2022-11-07 11:13:43
+ * @LastEditors: pengrenchang
+ * @LastEditTime: 2022-11-07 11:13:43
+ */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+});
